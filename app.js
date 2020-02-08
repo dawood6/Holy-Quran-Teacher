48f8456b0bf8bf8badcf024fc19c073957ba9ed3
dawood6 <dawoodsadiq171@gmail.com>
2020-02-08 21:16:02 +0500

    Holy Quran Teacher With JavaScript

---
+++ app.js
@@ -0,0 +1,17 @@
+const one = new Audio();
+one.src = "1.mp3";
+
+const two = new Audio();
+two.src = "2.mp3";
+
+const three = new Audio();
+three.src = "3.mp3";
+
+const four = new Audio();
+four.src = "4.mp3";
+
+const five = new Audio();
+five.src = "5.mp3";
+
+const six = new Audio();
+six.src = "6.mp3";
